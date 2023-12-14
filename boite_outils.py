9f3efab7efe97276456d8417c8d45dc65933e944
MaillotCedric <cedricmaillot38@gmail.com>
2023-12-14 18:58:46 +0400

    Add boite_outils.py

---
+++ boite_outils.py
@@ -0,0 +1,6 @@
+import os
+import pandas as pd
+
+def save_dataframe(dataframe: pd.DataFrame, nom_fichier: str, emplacement_relatif_fichier: str="./"):
+    dataframe_path = os.path.join(emplacement_relatif_fichier, nom_fichier)
+    dataframe.to_pickle(dataframe_path)
