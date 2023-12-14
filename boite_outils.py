408353c4caf4cbf608fecf6997f2b1e782172cc2
MaillotCedric <cedricmaillot38@gmail.com>
2023-12-14 20:05:22 +0400

    Fix boite_outils.py

--- boite_outils.py
+++ boite_outils.py
@@ -2,5 +2,5 @@ import os
 import pandas as pd
 
 def save_dataframe(dataframe: pd.DataFrame, nom_fichier: str, emplacement_relatif_fichier: str="./"):
-    dataframe_path = os.path.join(emplacement_relatif_fichier, nom_fichier)
+    dataframe_path = os.path.realpath(f"{emplacement_relatif_fichier}{nom_fichier}")
     dataframe.to_pickle(dataframe_path)
