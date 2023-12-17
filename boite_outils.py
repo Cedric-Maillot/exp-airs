0d5823c4cc42abf4cf71b8e7346a0bac8aeec3af
MaillotCedric <cedricmaillot38@gmail.com>
2023-12-17 15:37:17 +0400

    Update boite_outils.py

--- boite_outils.py
+++ boite_outils.py
@@ -13,15 +13,13 @@ def save_dataframe(dataframe: pd.DataFrame, nom_fichier: str, emplacement_relati
 def time_series_concentration_polluant(
         df: pd.DataFrame,
         date_debut: str,
-        date_fin: str
+        date_fin: str,
+        nom_station: str,
+        nom_polluant: str,
+        seuil_information_recommendation: int,
+        seuil_alerte: int
     ) -> plt.graph_objs.Figure:
     px = plt.express
-
-    # dates au format "yyyy/mm/dd"
-    # date_debut = "2023/07/01"
-    # date_fin = "2023/07/31"
-    nom_station = "Bourg Murat"
-    nom_polluant = "Dioxyde de soufre"
 
     filtered_df = df.loc[(df["date_debut"] >= date_debut) & (df["date_debut"] <= date_fin)]
     filtered_df = filtered_df.query(f"nom_station=='{nom_station}'")
@@ -29,7 +27,7 @@ def time_series_concentration_polluant(
 
     filtered_df = filtered_df.sort_values(by="date_debut")
 
-    fig = px.line(filtered_df, x="date_debut", y="valeur", title="Évolution de la concentration de S02")
+    fig = px.line(filtered_df, x="date_debut", y="valeur", title=f"Évolution de la concentration de {nom_polluant}")
 
     fig.update_layout(
         xaxis_title="Date et heure",
@@ -37,16 +35,29 @@ def time_series_concentration_polluant(
     )
 
     # seuil d'information et de recommendation (plotly version 5.14 or higher)
-    fig.add_hline(y=300, line_width=1, line_color="rgb(256, 126, 0)", label=dict(
-        text="seuil d'information et de recommendation",
-        textposition="bottom right",
-        font=dict(color="rgb(256, 126, 0)")
-    ))
+    fig.add_hline(
+        y=seuil_information_recommendation,
+        line_width=1,
+        line_color="rgb(256, 126, 0)",
+        label=dict(
+            text="seuil d'information et de recommendation",
+            textposition="bottom right",
+            font=dict(
+                color="rgb(256, 126, 0)"
+            )
+        )
+    )
     # seuil d'alerte (plotly version 5.14 or higher)
-    fig.add_hline(y=500, line_width=1, line_color="rgb(204, 0, 0)", label=dict(
-        text="seuil d'alerte",
-        textposition="top right",
-        font=dict(color="rgb(204, 0, 0)")
-    ))
+    fig.add_hline(y=seuil_alerte,
+        line_width=1,
+        line_color="rgb(204, 0, 0)",
+        label=dict(
+            text="seuil d'alerte",
+            textposition="top right",
+            font=dict(
+                color="rgb(204, 0, 0)"
+            )
+        )
+    )
 
     return fig
